6b88c4672921ab561d1af34d0200a65bcca1ef1b
Rachel Xiao <rachelxiao907@gmail.com>
2020-11-22 12:10:28 -0500

    Added constructor

---
+++ RationalNumber.java
@@ -0,0 +1,18 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno) {
+    super(0.0);
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    } else if (deno < 0) {
+      numerator = nume * -1;
+      denominator = deno * -1;
+    } else {
+      numerator = nume;
+      denominator = deno;
+    }
+  }
+  
+}

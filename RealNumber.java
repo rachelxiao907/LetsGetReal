fc44e29945f4359d2ec0ed21be28861cb0d10472
Rachel Xiao <rachelxiao907@gmail.com>
2020-11-26 18:16:05 -0500

    RealNumber class will just use the inherited .equals() method

--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,4 @@
-public class RealNumber {
+public class RealNumber extends Number {
   private double value;
 
   public RealNumber(double v){
@@ -11,13 +11,6 @@ public class RealNumber {
 
   public String toString(){
     return ""+getValue();
-  }
-
-  public boolean equals(RealNumber other) {
-    if (value == 0 || other.getValue() == 0) {
-      return value == 0 && other.getValue() == 0;
-    }
-    return Math.abs((other.getValue() - value) / value * 100) < 0.001;
   }
 
   public RealNumber add(RealNumber other) {
